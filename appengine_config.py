b568d00410ae3142ade39ba7a656add427873d89
DFectuoso <santiago1717@gmail.com>
2011-03-04 13:01:54 -0800

    After big stress test

--- appengine_config.py
+++ appengine_config.py
@@ -1,4 +1,4 @@
 from gaesessions import SessionMiddleware
 def webapp_add_wsgi_middleware(app):
-    app = SessionMiddleware(app, cookie_key="VepaR94rE2u2ucHU6Efrapucabruc2wrewrebAcayuSubufuwe3Ta7uYEzaTeY7fz")
+    app = SessionMiddleware(app, cookie_key="FDAVepaR94rE2u2ucHU6Efrapucabruc2wrewrebAcayuSubufuwe3Ta7uYEzaTeY7fz")
     return app
